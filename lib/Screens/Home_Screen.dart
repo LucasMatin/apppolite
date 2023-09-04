daf584c9253f53a0fa9cd6d6676f774ad1acbf20
LucasMatin <you@example.com>
2023-09-04 22:34:43 +0700

    again

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -12,47 +12,49 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.brown[300],
-        elevation: 0,
-        title: Text(
-          'รายการการบริโภค',
-          style: TextStyle(color: Colors.white, fontSize: 23),
-        ),
-        centerTitle: true,
-      ),
-      body: Column(children: [
-        Container(
-          height: 75,
-          color: Color.fromARGB(255, 228, 203, 184),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 520),
-          child: Container(
-            width: 260,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const Foodscreen(),
-                  ),
-                );
-              },
-              style: ButtonStyle(
-                minimumSize: MaterialStateProperty.all(const Size(
-                  double.infinity,
-                  48,
-                )),
-              ),
-              child: const Text(
-                'บันทึกเมนูอาหาร',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-              ),
-            ),
+        appBar: AppBar(
+          backgroundColor: Colors.brown[300],
+          elevation: 0,
+          title: Text(
+            'รายการการบริโภค',
+            style: TextStyle(color: Colors.white, fontSize: 23),
           ),
-        )
-      ]),
-    );
+          centerTitle: true,
+        ),
+        body: SafeArea(
+          child: Column(children: [
+            Container(
+              height: 75,
+              color: Color.fromARGB(255, 228, 203, 184),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 520),
+              child: Container(
+                width: 260,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const Foodscreen(),
+                      ),
+                    );
+                  },
+                  style: ButtonStyle(
+                    minimumSize: MaterialStateProperty.all(const Size(
+                      double.infinity,
+                      48,
+                    )),
+                  ),
+                  child: const Text(
+                    'บันทึกเมนูอาหาร',
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+                  ),
+                ),
+              ),
+            )
+          ]),
+        ));
   }
 }
