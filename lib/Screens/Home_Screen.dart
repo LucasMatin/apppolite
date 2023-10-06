e23f8ed8ccf16c5ac089b3745cf07794832028f4
LucasMatin <you@example.com>
2023-10-06 23:47:47 +0700

    aaaaaaaaaaaaaa

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names, sized_box_for_whitespace
+// ignore_for_file: file_names, sized_box_for_whitespace, unused_local_variable, avoid_print
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
